f650702f588568c4f02f996130e2f243a0aad227
asus <devmailbox27@gmail.com>
2021-08-03 19:46:39 +0530

    recursion all

---
+++ reverse.py
@@ -0,0 +1,10 @@
+word = input("Enter a string: ")
+
+
+def reverse(string):
+    if len(string) <= 1:
+        return string
+    return string[-1] + reverse(string[0:-1])
+
+
+print(f"Reverse of string: {reverse(word)}")
